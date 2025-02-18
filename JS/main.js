dd10ada021327c432ccc2b64caec0e0b5aafcae8
NivedDoesArt <devindewit2004@gmail.com>
2025-02-18 11:39:43 +0100

    18-02-2025 2

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,17 @@
 window.onbeforeunload = function() {
     window.scrollTo(0, 0);
 };
+
+
+
+async function navbar(location) {
+    document.getElementById('body').style.opacity = '0%'
+    await new Promise(resolve => setTimeout(resolve, 500));
+    window.location.href = location
+}
+
+
+
+window.onload = async function() {
+    document.getElementById('body').style.opacity = '100%'
+}
