cb7a41b4d830d705784bb7b4ba6363e6c48ce9b8
NivedDoesArt <devindewit2004@gmail.com>
2025-04-02 16:19:11 +0200

    added action warnings and more bio info

--- JS/premade.js
+++ JS/premade.js
@@ -1,3 +1,5 @@
 function openSong(value) {
     window.location.href = "../index.html#" + value
 }
+
+document.getElementById("content").style.height = "calc(" + (document.getElementById("middle").offsetHeight * 2) + "px + -20vw)"
