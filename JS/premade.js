731dff0a5c30fca4a70cf669cc4d82b42bccaa4f
NivedDoesArt <devindewit2004@gmail.com>
2025-04-03 16:52:57 +0200

    smooth scroll into view on premade tab

--- JS/premade.js
+++ JS/premade.js
@@ -3,3 +3,7 @@ function openSong(value) {
 }
 
 document.getElementById("content").style.height = "calc(" + (document.getElementById("middle").offsetHeight * 2) + "px + -20vw)"
+
+document.getElementById("middle").addEventListener("scroll", (event) => {
+    document.getElementById("middle").scrollIntoView({behavior: "smooth", block: "center"})
+})
